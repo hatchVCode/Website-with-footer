fff7a0fba62e65968d3865e07c3f23a349a511c6
hatchVCode <75653400+hatchVCode@users.noreply.github.com>
2021-09-20 00:16:35 -0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(225);
+    fill(35, 50, 240);
+    ellipse(mouseX, mouseY, 50, 50);
+    ellipse(mouseX+0, mouseY+13, 40, 10);
+    ellipse(mouseX, mouseY, 10, 10);
+    ellipse(mouseX, mouseY, 10, 10);
+    ellipse(mouseX+10, mouseY-10, 10, 10);
+    ellipse(mouseX-10, mouseY-10, 10, 10);
+}
+© 2021 GitHub, Inc.
